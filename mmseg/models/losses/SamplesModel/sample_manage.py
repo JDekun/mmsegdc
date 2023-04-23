9bfac120feda918c556ccb4e3bdd27c21fc03894
JDekun <jurry_k@outlook.com>
2023-04-23 19:02:44 +0800

    de

--- mmseg/models/losses/SamplesModel/sample_manage.py
+++ mmseg/models/losses/SamplesModel/sample_manage.py
@@ -34,22 +34,23 @@ def Sampling(type, X, Y, labels, predict, ignore_label: int = 255):
                 num_h = hard_indices.shape[0]
                 num_e = easy_indices.shape[0]
                 if num_e == 0:
-                    ade_x = torch.mean(X[ii, hard_indices, :].squeeze(1), dim=0)*hard_weight
-                    ade_y = torch.mean(Y[ii, hard_indices, :].squeeze(1), dim=0)*hard_weight
+                    ade_x = torch.mean(X[ii, hard_indices, :], dim=0)*hard_weight
+                    ade_y = torch.mean(Y[ii, hard_indices, :], dim=0)*hard_weight
                 elif num_h == 0:
-                    ade_x = torch.mean(X[ii, easy_indices, :].squeeze(1), dim=0)*easy_weight
-                    ade_y = torch.mean(Y[ii, easy_indices, :].squeeze(1), dim=0)*easy_weight
+                    ade_x = torch.mean(X[ii, easy_indices, :], dim=0)*easy_weight
+                    ade_y = torch.mean(Y[ii, easy_indices, :], dim=0)*easy_weight
                 else:
-                    ade_x = torch.mean(X[ii, hard_indices, :].squeeze(1), dim=0)*hard_weight  + torch.mean(X[ii, easy_indices, :].squeeze(1), dim=0)*easy_weight
-                    ade_y = torch.mean(Y[ii, hard_indices, :].squeeze(1), dim=0)*hard_weight  + torch.mean(Y[ii, easy_indices, :].squeeze(1), dim=0)*easy_weight
+                    ade_x = torch.mean(X[ii, hard_indices, :], dim=0)*hard_weight  + torch.mean(X[ii, easy_indices, :].squeeze(1), dim=0)*easy_weight
+                    ade_y = torch.mean(Y[ii, hard_indices, :], dim=0)*hard_weight  + torch.mean(Y[ii, easy_indices, :].squeeze(1), dim=0)*easy_weight
                 X_[X_ptr, 0, :] = ade_x
                 Y_[X_ptr, 0, :] = ade_y
                 y_[X_ptr] = cls_id
                 X_ptr += 1
 
                 print('ade_x:', ade_x)
-                print('num_h:', num_h)
-                print('num_e:', num_e)
+                print('sum:', torch.sum(X[ii, :, :], dim=0))
+                print('sum:', X)
+                
             else:  
                 if "adapt_excite" in type:
                     n = int(type.split('_')[-1])
@@ -59,8 +60,8 @@ def Sampling(type, X, Y, labels, predict, ignore_label: int = 255):
 
                 temp = indices.shape[0]
                 if temp != 0:
-                    X_[X_ptr, 0, :] = torch.mean(X[ii, indices, :].squeeze(1), dim=0)
-                    Y_[X_ptr, 0, :] = torch.mean(Y[ii, indices, :].squeeze(1), dim=0)
+                    X_[X_ptr, 0, :] = torch.mean(X[ii, indices, :], dim=0)
+                    Y_[X_ptr, 0, :] = torch.mean(Y[ii, indices, :], dim=0)
                     y_[X_ptr] = cls_id
                 X_ptr += 1
 
