175473922b8510412e2b2a3d4654f2b4ed293de2
JDekun <jurry_k@outlook.com>
2023-04-23 20:50:03 +0800

    de

--- mmseg/models/decode_heads/ocr_head_contrast.py
+++ mmseg/models/decode_heads/ocr_head_contrast.py
@@ -170,10 +170,14 @@ class OCRHead_CON(BaseCascadeDecodeHeadConTrast):
         context = self.spatial_gather_module(feats, prev_output)
         object_context = self.object_context_block(feats, context)
 
-        feats = object_context.permute(0, 2, 3, 1)
+        feats = feats.permute(0, 2, 3, 1)
         feats = feats.contiguous().view(feats.shape[0], -1, feats.shape[-1])
         for ii in range(feats.shape[0]):
-            print('sum:', torch.sum(feats[ii, :, :], dim=1))
+            print('feats:', torch.sum(feats[ii, :, :], dim=1))
+        feats = context.permute(0, 2, 3, 1)
+        feats = feats.contiguous().view(feats.shape[0], -1, feats.shape[-1])
+        for ii in range(feats.shape[0]):
+            print('context:', torch.sum(feats[ii, :, :], dim=1))
 
         output = OrderedDict()
         # >>> project contrast
