74f70a054eb3db4e432a31b3d1f3f7c662117f8e
JDekun <jurry_k@outlook.com>
2023-04-23 11:27:50 +0800

    添加新的ocr+contrats配置文件

---
+++ configs/ocrnet/con_ocrnet_r101-d8_4xb2-40k_cityscapes-512x1024.py
@@ -0,0 +1,36 @@
+_base_ = [
+    '../_base_/models/ocrnet_r50-d8.py', '../_base_/datasets/cityscapes.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
+]
+crop_size = (512, 1024)
+data_preprocessor = dict(size=crop_size)
+model = dict(
+    data_preprocessor=data_preprocessor,
+    pretrained='open-mmlab://resnet101_v1c',
+    backbone=dict(depth=101),
+    decode_head=[
+        dict(
+            type='FCNHead',
+            in_channels=1024,
+            in_index=2,
+            channels=256,
+            num_convs=1,
+            concat_input=False,
+            dropout_ratio=0.1,
+            num_classes=19,
+            align_corners=False,
+            loss_decode=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+        dict(
+            type='OCRHead_CON',
+            in_channels=2048,
+            in_index=3,
+            channels=512,
+            ocr_channels=256,
+            dropout_ratio=0.1,
+            num_classes=19,
+            align_corners=False,
+            loss_decode=dict(
+                type='ContrastCrossEntropyLoss', use_sigmoid=False, loss_weight=1.0))
+    ]
+    )
