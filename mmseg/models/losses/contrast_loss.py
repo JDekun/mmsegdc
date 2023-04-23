02cb0beba0297467c05dc2482fdad65d15d77c30
JDekun <jurry_k@outlook.com>
2023-04-23 16:25:31 +0800

    debug Nan

--- mmseg/models/losses/contrast_loss.py
+++ mmseg/models/losses/contrast_loss.py
@@ -175,7 +175,7 @@ def Contrastive(feats_x, feats_y, labels_, queue=None, queue_label=None, type: s
 
 
     # 计算对比logits
-    anchor_dot_contrast = torch.div((anchor_feature, torch.transpose(contrast_feature, 0, 1)), temperature)
+    anchor_dot_contrast = torch.div(torch.matmul(anchor_feature, torch.transpose(contrast_feature, 0, 1)), temperature)
     logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
     logits = anchor_dot_contrast - logits_max.detach()
     # logits = anchor_dot_contrast
