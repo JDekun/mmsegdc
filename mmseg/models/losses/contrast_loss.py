34fbf5958f33fb46b6c84503c2643de3a429ee69
JDekun <jurry_k@outlook.com>
2023-04-23 15:19:46 +0800

    debug Nan

--- mmseg/models/losses/contrast_loss.py
+++ mmseg/models/losses/contrast_loss.py
@@ -208,7 +208,8 @@ def Contrastive(feats_x, feats_y, labels_, queue=None, queue_label=None, type: s
     loss = - (temperature / base_temperature) * mean_log_prob_pos
     loss = loss.mean()
     
-    print('labels_:', labels_)
+    print('logits:', logits)
+    print('exp_logits:', exp_logits)
 
     return loss
 
