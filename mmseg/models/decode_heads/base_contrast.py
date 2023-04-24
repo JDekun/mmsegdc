81efa60b07a1f15b8ec1328fb9cefabf920a526e
JDekun <jurry_k@outlook.com>
2023-04-24 21:52:22 +0800

    de

--- mmseg/models/decode_heads/base_contrast.py
+++ mmseg/models/decode_heads/base_contrast.py
@@ -6,7 +6,7 @@ class EncodeProjector(nn.Module):
     def __init__(self, decode_channels, layers, proj_channels, conv_cfg, norm_cfg, act_cfg):
         super().__init__()
         self.layers = layers
-        self.decode = nn.sequence(
+        self.decode = nn.Sequential(
             ConvModule(decode_channels, decode_channels, 1,
                         conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg),
             ConvModule(decode_channels, proj_channels, 1,
@@ -14,25 +14,25 @@ class EncodeProjector(nn.Module):
         self.layers_proj = OrderedDict()
         for layer in self.layers:
             if layer == 'layer_4':
-                self.layers_proj['layer_4'] = nn.sequence(
+                self.layers_proj['layer_4'] = nn.Sequential(
                     ConvModule(2048, decode_channels, 1,
                                 conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg),
                     ConvModule(decode_channels, proj_channels, 1,
                                 conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg))
             if layer == 'layer_3':
-                self.layers_proj['layer_3'] = nn.sequence(
+                self.layers_proj['layer_3'] = nn.Sequential(
                     ConvModule(1024, decode_channels, 1,
                                 conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg),
                     ConvModule(decode_channels, proj_channels, 1,
                                 conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg))
             if layer == 'layer_2':
-                self.layers_proj['layer_2'] = nn.sequence(
+                self.layers_proj['layer_2'] = nn.Sequential(
                     ConvModule(512, decode_channels, 1,
                                 conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg),
                     ConvModule(decode_channels, proj_channels, 1,
                                 conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg))
             if layer == 'layer_1':
-                self.layers_proj['layer_1'] = nn.sequence(
+                self.layers_proj['layer_1'] = nn.Sequential(
                     ConvModule(256, 256, 1,
                                 conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg),
                     ConvModule(256, proj_channels, 1,
