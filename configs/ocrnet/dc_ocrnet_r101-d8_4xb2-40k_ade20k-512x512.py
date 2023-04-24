0c8b31b80abb37c92a2c774084c8e4513153bb2e
JDekun <jurry_k@outlook.com>
2023-04-24 16:05:22 +0800

    de

--- configs/ocrnet/dc_ocrnet_r101-d8_4xb2-40k_ade20k-512x512.py
+++ configs/ocrnet/dc_ocrnet_r101-d8_4xb2-40k_ade20k-512x512.py
@@ -2,6 +2,7 @@ _base_ = [
     '../_base_/models/ocrnet_r50-d8_dc.py', '../_base_/datasets/ade20k.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
 ]
+norm_cfg = dict(type='SyncBN', requires_grad=True)
 crop_size = (512, 512)
 data_preprocessor = dict(size=crop_size)
 model = dict(
@@ -9,10 +10,30 @@ model = dict(
     pretrained='open-mmlab://resnet101_v1c',
     backbone=dict(depth=101),
     decode_head=[
-        dict(num_classes=150
-            ),
         dict(
-            num_classes=150
-            )
+            type='FCNHead',
+            in_channels=1024,
+            in_index=2,
+            channels=256,
+            num_convs=1,
+            concat_input=False,
+            dropout_ratio=0.1,
+            num_classes=150,
+            norm_cfg=norm_cfg,
+            align_corners=False,
+            loss_decode=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+        dict(
+            type='OCRHead_DC',
+            in_channels=2048,
+            in_index=3,
+            channels=512,
+            ocr_channels=256,
+            dropout_ratio=0.1,
+            num_classes=150,
+            norm_cfg=norm_cfg,
+            align_corners=False,
+            loss_decode=dict(
+                type='CrossEntropyLossDC', use_sigmoid=False, loss_weight=1.0))
     ]
     )
