88ebfb66db841d4a21bc855dafb0e159f6098e22
JDekun <jurry_k@outlook.com>
2023-04-24 16:48:42 +0800

    删除一些不必要的语义分割头

---
+++ configs/dcnet/dc_deeplabv3_r50-d8_4xb2-40k_cityscapes-512x1024.py
@@ -0,0 +1,22 @@
+_base_ = [
+    '../_base_/models/deeplabv3_r50-d8.py', '../_base_/datasets/cityscapes.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
+]
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+crop_size = (512, 1024)
+data_preprocessor = dict(size=crop_size)
+model = dict(data_preprocessor=data_preprocessor,
+             pretrained='open-mmlab://resnet101_v1c', 
+             backbone=dict(depth=101),
+             decode_head=dict(
+                type='ASPPHead',
+                in_channels=2048,
+                in_index=3,
+                channels=512,
+                dilations=(1, 12, 24, 36),
+                dropout_ratio=0.1,
+                num_classes=19,
+                norm_cfg=norm_cfg,
+                align_corners=False,
+                loss_decode=dict(
+                    type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)))
