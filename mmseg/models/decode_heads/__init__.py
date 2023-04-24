27848fac42fdb34397ad76f99738bf40e519db14
JDekun <jurry_k@outlook.com>
2023-04-24 17:15:55 +0800

    de

--- mmseg/models/decode_heads/__init__.py
+++ mmseg/models/decode_heads/__init__.py
@@ -12,12 +12,7 @@ from .ocr_head_dcnet import OCRHead_DC
 from .aspp_head_dc import ASPPHeadDC
 
 __all__ = [
-    'FCNHead', 'PSPHead', 'ASPPHead', 'PSAHead', 'NLHead', 'GCHead', 'CCHead',
-    'UPerHead', 'DepthwiseSeparableASPPHead', 'ANNHead', 'DAHead', 'OCRHead',
-    'EncHead', 'DepthwiseSeparableFCNHead', 'FPNHead', 'EMAHead', 'DNLHead',
-    'PointHead', 'APCHead', 'DMHead', 'LRASPPHead', 'SETRUPHead',
-    'SETRMLAHead', 'DPTHead', 'SETRMLAHead', 'SegmenterMaskTransformerHead',
-    'SegformerHead', 'ISAHead', 'STDCHead', 'IterativeDecodeHead',
-    'KernelUpdateHead', 'KernelUpdator', 'MaskFormerHead', 'Mask2FormerHead',
-    'LightHamHead', 'PIDHead', 'OCRHead_CON', 'OCRHead_DC', 'ASPPHeadDC'
+    'FCNHead', 'PSPHead', 'ASPPHead', 'FPNHead', 'OCRHead',
+    'PSAHead', 'DepthwiseSeparableASPPHead', 'DepthwiseSeparableFCNHead',
+    'OCRHead_DC', 'ASPPHeadDC'
 ]
