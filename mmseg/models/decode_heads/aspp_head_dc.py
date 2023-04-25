ea8121f1e8d0537b25b706330433a05fa2532735
JDekun <jurry_k@outlook.com>
2023-04-25 10:21:19 +0800

    de

--- mmseg/models/decode_heads/aspp_head_dc.py
+++ mmseg/models/decode_heads/aspp_head_dc.py
@@ -8,7 +8,78 @@ from ..utils import resize
 from .decode_head import BaseDecodeHead
 
 from collections import OrderedDict
-from .base_contrast import EncodeProjector
+# from .base_contrast import EncodeProjector
+
+class Encode(nn.Module):
+    def __init__(self, layer_channels, ocr_channels, proj_channels, conv_cfg, norm_cfg, act_cfg):
+        super().__init__()
+        self.bottleneck = ConvModule(
+            layer_channels,
+            ocr_channels,
+            1,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg,
+            act_cfg=act_cfg)
+        self.projector = ConvModule(
+            ocr_channels,
+            proj_channels,
+            1,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg,
+            act_cfg=act_cfg)
+        
+    def forward(self, feats):
+        feats = self.bottleneck(feats)
+        feats = self.projector(feats)
+
+        return feats
+
+class EncodeProjector(nn.Module):
+    def __init__(self, decode_channels, layers, proj_channels, conv_cfg, norm_cfg, act_cfg):
+        super().__init__()
+        self.layers = layers
+        self.decode = Encode(decode_channels, decode_channels, proj_channels,
+                        conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg)
+        self.layers_proj = OrderedDict()
+        for layer in self.layers:
+            if layer == 'layer_4':
+                self.layers_proj['layer_4'] = Encode(2048, decode_channels, proj_channels,
+                        conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg)
+            if layer == 'layer_3':
+                self.layers_proj['layer_3'] = Encode(1024, decode_channels, proj_channels,
+                        conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg)
+            if layer == 'layer_2':
+                self.layers_proj['layer_2'] = Encode(512, decode_channels, proj_channels,
+                        conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg)
+            if layer == 'layer_1':
+                self.layers_proj['layer_1'] = Encode(256, 256, proj_channels,
+                        conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg)
+        
+        self.de_projector = ConvModule(
+                proj_channels,
+                decode_channels,
+                1,
+                conv_cfg=conv_cfg,
+                norm_cfg=norm_cfg,
+                act_cfg=None)
+        # self.cov1 = ConvModule(
+        #         self.channels,
+        #         self.channels,
+        #         1,
+        #         conv_cfg=self.conv_cfg,
+        #         norm_cfg=self.norm_cfg,
+        #         act_cfg=self.act_cfg)
+        
+    def forward(self, decode, inputs):
+        proj = OrderedDict()
+        decode = self.decode(decode)
+        con = self.de_projector(decode)
+
+        for layer in self.layers:
+            index = int(layer.split('_')[-1]) - 1
+            proj[layer] = self.layers_proj[layer](inputs[index])
+
+        return decode, proj, con
 
 class ASPPModule(nn.ModuleList):
     """Atrous Spatial Pyramid Pooling (ASPP) Module.
